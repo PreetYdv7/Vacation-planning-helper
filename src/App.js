20a02b1f203020b88c51d0179317e1dcaf724d34
Preet Yadav <133802505+PreetYdv7@users.noreply.github.com>
2025-01-16 13:09:31 +0530

    Made the no. of Items and Item place holder functional

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
   { id: 2, description: "Socks", quantity: 12, packed: false },
@@ -20,20 +22,43 @@ const Logo = () => {
 };
 
 const Form = () => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    setDescription("");
+    setQuantity(1);
   };
+
   return (
     <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What stuff do you need for the trip ⁉️</h3>
-      <select>
+      <h3>What stuff do you need for the trip 😎 ⁉️</h3>
+
+      <select
+        value={quantity}
+        onChange={(n) => setQuantity(Number(n.target.value))}
+      >
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..."></input>
+
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+
       <button>ADD</button>
     </form>
   );
