54eb765d5e4ff3cc482814abc830d136e0a1aa18
Preet Yadav <133802505+PreetYdv7@users.noreply.github.com>
2025-01-23 00:22:04 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -109,16 +109,27 @@ const Item = ({ item, onDeleteItems, onTogglePacked }) => {
 };
 
 const Progess = ({ items }) => {
+  if (!items.length)
+    return (
+      <p className="stats">
+        <em>Start adding items to your packing list</em>
+      </p>
+    );
+
   const total = items.length;
   const packed = items.filter((item) => item.packed).length;
   const percentage = total > 0 ? Math.round((packed / total) * 100) : 0;
 
   return (
     <footer className="stats">
-      <em>
-        You have {total} items on your list, and you have already packed{" "}
-        {packed} ({percentage}%)
-      </em>
+      {percentage === 100 ? (
+        "You are ready to go ✈️"
+      ) : (
+        <em>
+          You have {total} items on your list, and you have already packed{" "}
+          {packed} ({percentage}%)
+        </em>
+      )}
     </footer>
   );
 };
