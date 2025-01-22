44b0ea0500f0da3dfdbb87b40831f424d7183ea8
Preet Yadav <133802505+PreetYdv7@users.noreply.github.com>
2025-01-23 01:11:44 +0530

    added sort option

--- src/App.js
+++ src/App.js
@@ -80,10 +80,24 @@ const Form = ({ onAdditems }) => {
 };
 
 const PackingItems = ({ items, onDeleteItems, onTogglePacked }) => {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+  else if (sortBy === "description")
+    sortedItems = [...items].sort((a, b) =>
+      a.description.localeCompare(b.description)
+    );
+  else if (sortBy === "packed")
+    sortedItems = [...items].sort(
+      (a, b) => Number(a.packed) - Number(b.packed)
+    );
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -92,6 +106,13 @@ const PackingItems = ({ items, onDeleteItems, onTogglePacked }) => {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 };
